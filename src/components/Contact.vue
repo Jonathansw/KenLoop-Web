661e5adc4bb1154471a58e1c6ecb7055b09ea1e0
jsw76 <jonathansw@outlook.com>
2018-01-26 18:11:30 -0800

    forgot got to add new files

---
+++ src/components/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ui divided item">
+    <div class="item">
+      <google-map name="homeLoc"></google-map>
+      <div class="middle aligned content">
+        <div class="ui big very relaxed list">
+          <div class="item">
+            <i class="users icon"></i>
+              <div class="content">
+                KDJFlk
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GoogleMap from './GoogleMap';
+
+export default {
+  name: 'Contact',
+  components: {
+    GoogleMap,
+  },
+};
+</script>
+<style>
+
+</style>
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="google-map" v-bind:id="mapName"></div>
+</template>
+
+<script>
+export default {
+  name: 'google-map',
+  props: ['name'],
+  data() {
+    return {
+      mapName: this.name + "-map",
+      markerCoord: [
+        {
+          latitude: 25.0340,
+          longitude: 121.5645,
+        },
+      ],
+    }
+  },
+  mounted() {
+    const element = document.getElementById(this.mapName);
+    const option = {
+      zoom: 16,
+      center: new google.maps.LatLng(25.0330, 121.5654)
+    };
+    const map = new google.maps.Map(element, option);
+    this.markerCoord.forEach((coord) => {
+      const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+      const marker = new google.maps.Marker({
+        position, 
+        map,
+      });
+    });
+  }
+};
+</script>
+<style scoped>
+.google-map {
+  width: 100%;
+  height: 500px;
+  margin: 0 auto;
+  background: gray;
+}
+</style>
